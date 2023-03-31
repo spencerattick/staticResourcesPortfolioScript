63b4417e630c11c104ea48f30d8d5e96a3d66570
Spencer Attick <spencer.attick@gmail.com>
2023-03-31 12:36:16 -0400

    add request to GoodReads

--- index.js
+++ index.js
@@ -8,17 +8,22 @@ const addPortfolioToDesktop = () => {
   exec('cd .. && git clone https://github.com/spencerattick/spencerattick.github.io.git && cd staticResourcesScript', (error, stdout, stderr) => {
     if (error) {
       console.error('\x1b[31m', 'There was an error changing directories or cloning the portfolio repo.');
-      console.log('STDERR: ', stderr);
-      console.log('ERRORRR: ', error);
+      console.log(stderr);
       return;
     }
     console.log('Cloned a new copy of portfolio repo to the Desktop.');
   })
 }
 
+const requestGoodReadsData = async () => {
+  console.log('Requesting data from GoodReads...');
+    const goodReads = await parse('https://medium.com/feed/@spencer.attick');
+    return JSON.stringify(goodReads);
+}
+
 
 const searchDesktopForPortfolio = () => {
-  exec('find ~/Desktop -type d -name "spencerattick.github.io"', (error, stdout, stderr) => {
+  exec('find ~/Desktop -type d -name "spencerattick.github.io"', async (error, stdout, stderr) => {
     console.log('\x1b[32m', 'Looking for portfolio repo in the Desktop...', '\x1b[0m');
     if (error) {
       console.error('\x1b[31m', `There was an error looking for the repo: ${error}`, '\x1b[0m');
@@ -27,17 +32,11 @@ const searchDesktopForPortfolio = () => {
       console.log('The repo does not currently exist on the Desktop.');
       //git pull the repo onto the desktop
       addPortfolioToDesktop();
-    } else if (stdout.length > 0) {
-      console.log('\x1b[35m', 'The portfolio repo is already on the Desktop.', '\x1b[0m');
-      //get the GoodReads and Medium feeds and update the repo
-      const getGoodReads = async () => {
-        console.log('Making the request to the GoodReads feed.');
-        const goodReads = await parse('https://medium.com/feed/@spencer.attick');
-          console.log(JSON.stringify(goodReads));
-      }
+    } 
+    //make the Goodreads request
+    console.log(await requestGoodReadsData());
+    //make the Medium request
 
-      // getGoodReads();
-    }
   });
 }
 
